6996af428110e64d0b27124259743267669cab92
Eugene Stahov <evgs@bombus-im.org>
2005-05-01 16:26:21 +0000

    modified   changelog.txt modified   evgs.bat В билд включена версия для моторолы (только jad)
    added      list_all.txt
    deleted    list_m55.txt
    added      moto.bat
    added      moto.jad
    modified   nbproject/project.properties
    modified   pack_release.bat
    modified   todo.txt

---
+++ moto.bat
@@ -0,0 +1,2 @@
+md dist\Release_Moto
+copy dist\Release\*.jad+moto.jad dist\Release_Moto\ /B
--- pack_release.bat
+++ pack_release.bat
@@ -1,3 +1,4 @@
 echo on
+call moto.bat
 del bombus*.rar
-rar32 a -s -agmm_dd bombus @list_m55.txt
+rar32 a -s -agmm_dd bombus @list_all.txt
