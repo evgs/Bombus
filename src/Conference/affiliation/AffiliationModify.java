dd7108ee806cdf47e9271081a9acabde85fdba5b
Eugene Stahov <evgs@bombus-im.org>
2005-11-03 17:19:50 +0000

    добавлен в репозиторий AffiliationModify.java

---
+++ src/Conference/affiliation/AffiliationModify.java
@@ -0,0 +1,108 @@
+/*
+ * AffiliationModify.java
+ *
+ * Created on 30 ќкт€брь 2005 г., 15:32
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package Conference.affiliation;
+
+import Client.StaticData;
+import com.alsutton.jabber.JabberDataBlock;
+import com.alsutton.jabber.JabberStream;
+import com.alsutton.jabber.datablocks.Iq;
+import javax.microedition.lcdui.*;
+import ui.YesNoAlert;
+
+/**
+ *
+ * @author EvgS
+ */
+public class AffiliationModify implements CommandListener{
+    
+    Display display;
+    Displayable parentView;
+    
+    Form f=new Form("Affiliation");
+    TextField jid;
+    ChoiceGroup affiliation;
+    
+    String room;
+    int recentAffiliation;
+    
+    Command cmdCancel=new Command("Cancel", Command.BACK, 99);
+    Command cmdOk=new Command("Set", Command.OK, 1);
+    
+    /** Creates a new instance of AffiliationModify */
+    public AffiliationModify(Display display, String room, String jid, String affiliation) {
+        this.display=display;
+        parentView=display.getCurrent();
+        
+        this.room=room;
+        this.jid=new TextField("Jid", jid, 80, TextField.URL);
+        f.append(this.jid);
+        
+        this.affiliation=new ChoiceGroup("Set affiliation to", ui.ConstMIDP.CHOICE_POPUP);
+        for (int index=0; index<=AffiliationItem.AFFILIATION_OUTCAST; index++) {
+            String name=AffiliationItem.getAffiliationName(index);
+            this.affiliation.append(name, null);
+            if (affiliation.equals(name)) recentAffiliation=index;
+        }
+        this.affiliation.setSelectedIndex(recentAffiliation, true);
+        f.append(this.affiliation);
+        
+        f.addCommand(cmdCancel);
+        f.addCommand(cmdOk);
+        f.setCommandListener(this);
+        display.setCurrent(f);
+    }
+    
+    
+    private void modify(){
+        JabberStream stream=StaticData.getInstance().roster.theStream;
+        
+        JabberDataBlock request=new Iq();
+        request.setTypeAttribute("set");
+        request.setAttribute("to", room);
+        request.setAttribute("id", "admin_modify");
+        JabberDataBlock query=request.addChild("query", null);
+        query.setNameSpace("http://jabber.org/protocol/muc#admin");
+        JabberDataBlock child=query.addChild("item", null);
+        child.setAttribute("jid", jid.getString());
+        child.setAttribute("affiliation", AffiliationItem.getAffiliationName(affiliation.getSelectedIndex()));
+        
+        
+        //processIcon(true);
+        //System.out.println(request.toString());
+        //stream.addBlockListener(this);
+        stream.send(request);
+        try {
+            Affiliations a=(Affiliations) parentView;
+            a.getList();
+        } catch (Exception e) {};
+        destroyView();
+    }
+    
+    public void commandAction(Command c, Displayable d) {
+        if (c==cmdOk) {
+            if (jid.size()==0) return;
+            if (recentAffiliation==AffiliationItem.AFFILIATION_OWNER) {
+                StringBuffer warn=new StringBuffer("Are You sure want to discard ");
+                warn.append(jid.getString());
+                warn.append(" from OWNER to ");
+                warn.append(AffiliationItem.getAffiliationName(affiliation.getSelectedIndex()));
+                new YesNoAlert(display, parentView, "Modify affiliation", warn.toString()) {
+                    public void yes() {
+                        modify();
+                    }
+                };
+            } else modify();
+        }
+        if (c==cmdCancel) { destroyView(); }
+    }
+    
+    private void destroyView() { display.setCurrent(parentView); }
+}
