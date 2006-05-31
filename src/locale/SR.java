d44344be9a65d9ee804d2d3bdd6e03e73f9db29f
Eugene Stahov <evgs@bombus-im.org>
2006-05-31 19:02:12 +0000

    changelog.txt

--- src/locale/SR.java
+++ src/locale/SR.java
@@ -229,7 +229,7 @@ public class SR {
     public final static String MS_YOU_HAVE_BEEN_INVITED="You have been invited to ";
     public final static String MS_SURE_CLEAR="Are You sure want to clear messagelist?";
     
-    public   static String MS_STORE_PRESENCE = loadString( "Store presence" );
+    public   static String MS_STORE_PRESENCE = loadString( "room presences" );
     
 
     public static String MS_XMLLANG;
