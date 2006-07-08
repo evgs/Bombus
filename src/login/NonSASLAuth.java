e561dbbc5a2a15ea58f3d705cfb398196256e73c
Eugene Stahov <evgs@bombus-im.org>
2006-07-08 16:28:37 +0000

    refactoring: login

---
+++ src/login/NonSASLAuth.java
@@ -0,0 +1,22 @@
+/*
+ * NonSASLAuth.java
+ *
+ * Created on 8 Июль 2006 г., 22:16
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package login;
+
+/**
+ *
+ * @author evgs
+ */
+public class NonSASLAuth {
+    
+    /** Creates a new instance of NonSASLAuth */
+    public NonSASLAuth() {
+    }
+    
+}
