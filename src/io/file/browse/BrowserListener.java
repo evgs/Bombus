e755c53fda0a4bb6a79a33f10021ded420619d48
Eugene Stahov <evgs@bombus-im.org>
2006-10-02 19:48:56 +0000

    forgotten file

---
+++ src/io/file/browse/BrowserListener.java
@@ -0,0 +1,18 @@
+/*
+ * BrowserListener.java
+ *
+ * Created on 2 Октябрь 2006 г., 1:44
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package io.file.browse;
+
+/**
+ *
+ * @author evgs
+ */
+public interface BrowserListener {
+    public void BrowserFilePathNotify(String pathSelected);
+}
