dada8d528c75832b845df674803204fb032e7f0e
Eugene Stahov <evgs@bombus-im.org>
2007-01-16 11:17:45 +0000

    invitations if already in room wouldn't change room status anymore

--- src/Client/Roster.java
+++ src/Client/Roster.java
@@ -1014,7 +1014,10 @@ public class Roster
                     String inviteReason=invite.getChildBlockText("reason");
                             
                     String room=from+'/'+sd.account.getNickName();
-                    initMuc(room, password).getConference().status=Presence.PRESENCE_OFFLINE;
+                    ConferenceGroup invConf=initMuc(room, password);
+                    
+                    if (invConf.getSelfContact().status==Presence.PRESENCE_OFFLINE)
+                        invConf.getConference().status=Presence.PRESENCE_OFFLINE;
                     
                     body=inviteFrom+SR.MS_IS_INVITING_YOU+from+" ("+inviteReason+')';
                     
