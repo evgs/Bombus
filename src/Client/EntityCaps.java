caa485ba7836a308f04c234364b5a8e7664b72a0
Eugene Stahov <evgs@bombus-im.org>
2007-08-11 07:19:55 +0000

    namespace and node fix for EntityCaps discovering

--- src/Client/EntityCaps.java
+++ src/Client/EntityCaps.java
@@ -30,12 +30,11 @@ public class EntityCaps implements JabberBlockListener{
         JabberDataBlock query=data.getChildBlock("query");
         if (query==null) return BLOCK_REJECTED;
         String node=query.getAttribute("node");
-        if (query.isJabberNameSpace("http://jabber.org/protocol/caps")) {
+        if (!query.isJabberNameSpace("http://jabber.org/protocol/disco#info")) 
+            return BLOCK_REJECTED;
+        if (node!=null) 
             if (!node.startsWith(BOMBUS_NAMESPACE))
                 return BLOCK_REJECTED;
-        } else if (query.isJabberNameSpace("http://jabber.org/protocol/disco#info")) {
-            if (node!=null) return BLOCK_REJECTED;
-        } else return BLOCK_REJECTED;
         
         JabberDataBlock result=new Iq(data.getAttribute("from"), Iq.TYPE_RESULT, data.getAttribute("id"));
         result.addChild(query);
@@ -73,7 +72,7 @@ public class EntityCaps implements JabberBlockListener{
         "http://jabber.org/protocol/muc",
 	"http://jabber.org/protocol/si",
 	"http://jabber.org/protocol/si/profile/file-transfer",
-	//"http://jabber.org/protocol/ibb"
+	"http://jabber.org/protocol/ibb"
 };
 
 }
