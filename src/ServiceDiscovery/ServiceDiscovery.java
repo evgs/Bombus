065c034f2b0e56582649b1b904226a27bd1a995c
Eugene Stahov <evgs@bombus-im.org>
2007-11-04 20:59:24 +0000

    * ServiceDiscovery: error message in alert box

--- src/ServiceDiscovery/ServiceDiscovery.java
+++ src/ServiceDiscovery/ServiceDiscovery.java
@@ -211,7 +211,7 @@ public class ServiceDiscovery
             
             XmppError xe=XmppError.findInStanza(data);
             
-            new AlertBox(xe.getName(), xe.getText(), null, display, this);
+            new AlertBox(data.getAttribute("from"), xe.toString(), null, display, this);
 
             return JabberBlockListener.BLOCK_PROCESSED;
         }
