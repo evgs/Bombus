315b6a7d98c9397249424c6f52fa7c7cbcb848f7
Eugene Stahov <evgs@bombus-im.org>
2008-02-17 16:08:15 +0000

    * XmlParser: correctly parsing of non-escaped single and quotes in xml attributes (like " ' " and ' " ') * XmlParser: correctly parsing of non-escaped single and double quotes in text section

--- src/com/alsutton/xmlparser/XMLParser.java
+++ src/com/alsutton/xmlparser/XMLParser.java
@@ -83,12 +83,13 @@ public class XMLParser
     int inQuote = 0;    // 0 or " or '
     boolean inXMLchar=false;
 
-    int nextChar = iostream.getNextCharacter();
-    if( nextChar == -1 )
-      throw new EndOfXMLException();
-    while( nextChar != -1)
-            //&& (inQuote == true || checker.shouldStop( nextChar ) == false) )
-    {
+    int nextChar;
+    
+    do {
+        nextChar = iostream.getNextCharacter();
+        
+        if (nextChar==-1) break;
+        
         if (nextChar==tagBracket)
             if (inQuote==0) break;
         
@@ -98,13 +99,14 @@ public class XMLParser
         switch (nextChar) {
             case '\'': // '
             case '\"': // "
-		if (tagBracket=='<') break;
-                inQuote=( inQuote==0 )? nextChar: 0;
-                break;
+		if (tagBracket=='<') break; //bypassing quotes to output
+                if (inQuote==0) { inQuote=nextChar; continue; } //extracting quoted attribute
+                if (inQuote==nextChar) { inQuote=0; continue; } //quoted string complete
+                break; //bypassing non-paired quote to output
             case '&':
                 inXMLchar=true;
                 xmlChar=new StringBuffer(6);
-                break;
+                continue;
             case ';':
                 if (inXMLchar) {
                     inXMLchar=false;
@@ -122,21 +124,29 @@ public class XMLParser
                             nextChar=' ';
                         }
                 }
-            default:
-                if (!inXMLchar) {
-                    if (streamData.length()<MAX_BLOCK_SIZE) 
-                        streamData.append( (char) nextChar );
-                    else if (streamData.length()==MAX_BLOCK_SIZE)
-                        streamData.append("...");
-                } else xmlChar.append( (char) nextChar );
         }
-        nextChar = iostream.getNextCharacter();
-    }
+        
+        if (inXMLchar) {
+            xmlChar.append( (char) nextChar );
+            continue;
+        }
+        
+        //appending character
+        if (streamData.length()<MAX_BLOCK_SIZE)
+            streamData.append( (char) nextChar );
+        else if (streamData.length()==MAX_BLOCK_SIZE)
+            streamData.append("...");
+        
+    } while (true);
     
+    if( nextChar == -1 && streamData.length()==0)
+      throw new EndOfXMLException();
+
     if( nextChar != '<' && nextChar != '>')
       streamData.append( (char) '\n' );
 
     String returnData = streamData.toString();
+    System.out.println(returnData);
     return returnData;
   }
 
